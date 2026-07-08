5c99dbde25a95ead8ea8269594de9e1ce8a91df7
Megha.Agarwal <megha.agarwal03@sap.com>
2026-07-08 09:40:20 +0530

    update pravin resume

---
+++ src/mockData/tempdata.js
@@ -0,0 +1,30 @@
+// {
+//   "company": "Mobiloitte Technologies, New Delhi",
+//   "roles": "Trainee",
+//   "duration": "Aug 2014 — Dec 2014",
+//   "projects": [
+//     {
+//       "title": "Hybrid Mobile Application Development",
+//       "bullets": [
+//         "Developed cross-platform mobile applications using AppCelerator Titanium and Ionic frameworks, maintaining clean MVC architecture with JavaScript and AngularJS.",
+//         "Integrated Google Maps APIs for geofenced push notifications and built interactive, low-latency messaging features using WebSockets."
+//       ],
+//       "techStack": "Cross Platform mobile application, AppCelerator, Titanium, MVC, JavaScript, Ionic Framework, AngularJS, Android and iOS mobile App."
+//     }
+//   ]
+// },
+// {
+//   "company": "NIIT Limited, New Delhi",
+//   "roles": "Intern",
+//   "duration": "Feb 2014 — Jul 2014",
+//   "projects": [
+//     {
+//       "title": "Web Component Development Using Java Technologies",
+//       "bullets": [
+//         "Engineered backend web components and managed data structures using Core Java, ensuring clean object-oriented design and efficient execution.",
+//         "Designed and consumed RESTful APIs to facilitate seamless data exchange between Java server modules and client-side applications."
+//       ],
+//       "techStack": "Core Java, JavaScript, RESTful API, Android and iOS mobile App."
+//     }
+//   ]
+// }
